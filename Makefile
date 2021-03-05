d302838367dc3cc16b90e041d7f8339f6a4987db
Peter H. Jin <peter@peterjin.org>
2021-03-05 16:52:36 -0600

    unix_scm_rights: Fix recvmsg returning 0 case, check for multiple SCM_RIGHTS messages

--- Makefile
+++ Makefile
@@ -3,13 +3,13 @@ CCLD ?= $(CC)
 CFLAGS ?= -fvisibility=hidden -Wall -O2 -fstack-protector-strong -fstack-clash-protection -D_FORTIFY_SOURCE=2
 CCLDFLAGS ?= -Wl,-z,relro -Wl,-z,now
 all: socketbox socketbox-inetd socketbox-relay send-receive-fd socket-query libsocketbox-preload.so run-with-socketbox
-socketbox: unix_scm_rights.o config_parser.o server.o lookup.o
+socketbox: unix_scm_rights.o config_parser.o server.o lookup.o libsocketbox.o
 	$(CCLD) $(CCLDFLAGS) -g -o $@ $^
 socketbox-inetd: socketbox-inetd.o unix_scm_rights.o libsocketbox.o
 	$(CCLD) $(CCLDFLAGS) -g -o $@ $^
 socketbox-relay: socketbox-relay.o unix_scm_rights.o libsocketbox.o
 	$(CCLD) $(CCLDFLAGS) -g -o $@ $^
-send-receive-fd: send-receive-fd.o unix_scm_rights.o
+send-receive-fd: send-receive-fd.o unix_scm_rights.o libsocketbox.o
 	$(CCLD) $(CCLDFLAGS) -g -o $@ $^
 socket-query: socket-query.o
 	$(CCLD) $(CCLDFLAGS) -g -o $@ $^
--- unix_scm_rights.c
+++ unix_scm_rights.c
@@ -1,26 +1,50 @@
-#include "unix_scm_rights.h"
+#include "libsocketbox.h"
 #include <sys/socket.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 #include <stddef.h>
+#define SCM_MAX_FD 256
 int skbox_receive_fd_from_socket(int fd) {
-	char data[128] = {0};
-	char anc_data[1280] = {0};
 	while (1) {
+		char data[128] = {0};
+		char anc_data[1280] = {0};
 		struct msghdr m = {NULL, 0, &(struct iovec) {data, 128}, 1, anc_data, 1280, 0};
 		ssize_t r = recvmsg(fd, &m, MSG_CMSG_CLOEXEC);
-		if (r <= 0) break;
+		if (r < 0) break;
+		if (r == 0) {
+			int sock_type = skbox_getsockopt_integer(fd, SOL_SOCKET, SO_TYPE);
+			if (sock_type < 0) {
+				errno = EINVAL;
+				return -1;
+			}
+			if (sock_type != SOCK_DGRAM) {
+				errno = ENOLINK;
+				return -1;
+			}
+		}
+		int has_fd_to_return = 0;
+		int fd_to_return = -1;
 		for (struct cmsghdr *c = CMSG_FIRSTHDR(&m); c; c = CMSG_NXTHDR(&m, c)) {
 			if (c->cmsg_level == SOL_SOCKET && c->cmsg_type == SCM_RIGHTS && c->cmsg_len >= sizeof(struct cmsghdr)) {
 				unsigned int nr_fds = (c->cmsg_len - sizeof(struct cmsghdr)) / sizeof(int);
-				if (nr_fds == 0 || nr_fds > 256) continue;
+				if (nr_fds == 0 || nr_fds > SCM_MAX_FD) continue;
 				int *fd_list = (int *) CMSG_DATA(c);
-				int fd_to_return = fd_list[0];
+				if (has_fd_to_return) {
+					for (unsigned int excess_fds = 0; excess_fds < nr_fds; excess_fds++) {
+						close(fd_list[excess_fds]);
+					}
+					continue;
+				}
+				fd_to_return = fd_list[0];
+				has_fd_to_return = 1;
 				for (unsigned int excess_fds = 1; excess_fds < nr_fds; excess_fds++) {
 					close(fd_list[excess_fds]);
 				}
-				return fd_to_return;
 			}
+		}
+		if (has_fd_to_return) {
+			return fd_to_return;
 		}
 	}
 	return -1;
